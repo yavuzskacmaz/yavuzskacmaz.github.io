e3fd2690bbe697091ddcd5abf56cb18ab9797e61
yavuzskacmaz <yavuzselimkacmaz@gmail.com>
2025-09-05 22:28:12 +0000

    Deploying to gh-pages from @ yavuzskacmaz/yavuzskacmaz.github.io@fd398e75320977c5649d199a85b175f79406868d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "nav-resume",
+  },{id: "nav-resume",
           title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
           },
         },{id: "post-a-post-with-plotly-js",
         
