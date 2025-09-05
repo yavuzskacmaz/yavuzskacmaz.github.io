4cf7126a21b73cdfc667201e3cb671f0fc34e569
yavuzskacmaz <yavuzselimkacmaz@gmail.com>
2025-09-05 22:29:44 +0000

    Deploying to gh-pages from @ yavuzskacmaz/yavuzskacmaz.github.io@704a73005b52a8ef9fa5edde06b74aaa0188c87f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Resume/";
+            window.location.href = "/cv/";
           },
         },{id: "nav-research",
           title: "Research",
